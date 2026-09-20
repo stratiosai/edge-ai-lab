01098d4c9cd136eacdccdefdd123373ac88e5182
Blake Tindol <stratiosai@gmail.com>
2026-09-19 22:41:52 -0400

    Allow camera capture to write the live FIFO

--- projects/private-ring-camera/pi/capture/run_capture.sh
+++ projects/private-ring-camera/pi/capture/run_capture.sh
@@ -26,6 +26,7 @@ rpicam-vid \
   --width 1920 --height 1080 --framerate 15 \
   --codec yuv420 -o - \
 | ffmpeg \
+  -y \
   -hide_banner -loglevel warning \
   -f rawvideo -pixel_format yuv420p -video_size 1920x1080 -framerate 15 -i pipe:0 \
   -an -map 0:v:0 \
